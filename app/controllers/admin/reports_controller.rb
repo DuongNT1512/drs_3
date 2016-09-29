a5a262c6a28faa176c1959b91362c409777e5481
yeunuthongminh <yeunuthongminh@gmail.com>
2016-09-29 22:06:08 +0700

    Ransack search

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,5 +1,14 @@
 class Admin::ReportsController < ApplicationController
   def index
-    @reports = Report.all.paginate page: params[:page], per_page: Settings.page
+    binding.pry
+    @search = Report.search params[:report]
+    @reports = @search.result.paginate page: params[:page], per_page: Settings.page
+    @search.build_condition
+  end
+
+  private
+  def report_params
+    params.require(:report).permit :division_id, :language_id, :working_day,
+      :progress_id, :achievement
   end
 end
