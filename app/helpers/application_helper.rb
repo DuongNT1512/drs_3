d00bec2acfb4138d147dd8b7a6c032e92f79bcf4
yeunuthongminh <yeunuthongminh@gmail.com>
2016-09-29 16:51:12 +0700

    Report policy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
   def current_user? user
     current_user == user
   end
+
+  def index_for_pages index, page
+    index = index + 1 + (page ? (page.to_i - 1) * Settings.page : 0)
+  end
 end
